0ab164b4185376a3fbabcc464c4e52799bf23eae
evgeny.rivkin <evgeny.rivkin@zerto.com>
2016-12-21 12:31:31 +0200

    [master] axios dependency
    Branch: [master]

--- app/core.js
+++ app/core.js
@@ -2,3 +2,8 @@
  * Created by evgeny.rivkin on 21/12/2016.
  * Edited By inbal.se
  */
+
+exports.core = () => {
+
+    return {};
+}
---
+++ ui/scripts/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by evgeny.rivkin on 21/12/2016.
+ */
+
+
