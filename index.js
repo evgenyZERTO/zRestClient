f2bb71fb087f99cc2cdc4579bc0b6c411a732f82
evgeny.rivkin <evgeny.rivkin@zerto.com>
2016-12-21 11:25:12 +0200

    [master] stubs
    Branch: [master]

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /**
  * Created by evgeny.rivkin on 21/12/2016.
  */
-//entry point
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
