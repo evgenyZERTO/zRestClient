3fec4a72be5b9ee4ec93bae88a02678a347d5fb2
Inbal Sertshuk Elkabetz <inbal.se@zerto.com>
2016-12-21 13:42:25 +0200

    [master] inbal
    Branch: [master]

--- app/rest-server.js
+++ app/rest-server.js
@@ -1,38 +1,30 @@
 /**
  * Created by evgeny.rivkin on 21/12/2016.
  */
+
 exports.auth =  (zvmip, zvmport, username, password) => {
-    var deferred = $q.defer();
-
-    axios({
-        method: 'post',
-        url: '/user/12345',
-        data: {
-            firstName: 'Fred',
-            lastName: 'Flintstone'
-        }
-    });
-    var authenticationRequest = {
-        method: 'POST',
-        url: 'https://' + zvmip + '/' + zvmport + '/v1/session/add',
-        headers: {
-            'Authorization': 'Basic ' + btoa(username + ':' + password)
-        },
-        data: {AuthenticationMethod: 1}
-    };
-
-    $http(authenticationRequest).success(function (data, status, headers/*, config*/) {
+    Axios.post('https://' + zvmip + '/' + zvmport + '/v1/session/add', 
+                {headers: {
+                            'Authorization': 'Basic ' + btoa(username + ':' + password)
+                        },
+                data: {AuthenticationMethod: 1}
+                })
+    .then((successResult) => {
         var session = headers('x-zerto-session');
         zertoApi.addHeaders({'x-zerto-session': session});
-
         console.debug('session identifier from login:' + globalStateModel.getSessionId());
         console.debug('session from rest:' + session);
-        deferred.resolve(data);
-
-    }).error(function (error) {
-        deferred.reject(error);
-        console.error('failed to authenticate to rest API');
+    }, (error) => {
+        return error;
     });
+};
 
-    return deferred.promise;
+exports.logout =  (zvmip, zvmport, sessionid) => {
+    Axios.delete('https://' + zvmip + '/' + zvmport + '/v1/session', 
+                {headers: {'x-zerto-session': sessionid}})
+    .then((successResult) => {
+        console.debug('Logged out - by deleting the session');
+    }, (error) => {
+        return error;
+    });
 };
