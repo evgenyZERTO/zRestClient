3e98d935264509143587b747313af17ff79f7314
Idan Shreiber <idan.shreiber@zerto.com>
2016-12-21 13:46:23 +0200

    [master] added authenticate function.
    Branch: [master]

--- app/rest-ui.js
+++ app/rest-ui.js
@@ -1,3 +1,19 @@
 /**
  * Created by evgeny.rivkin on 21/12/2016.
  */
+var restServer = require('rest-server');
+
+exports.authenticate = (ip, port, username, password) => {
+    return restServer.authenticate(ip, port, username, password);
+}
+
+exports.authenticate = (authenticationParams) => {
+    var tokensArray = [];
+
+    authenticationParams.forEach(function(ap)
+    {
+        console.log(JSON.stringify(ap));
+
+        authenticate(ap.ip, ap.port, ap.username, ap.password);
+    });
+}
