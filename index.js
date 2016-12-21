0d4634247f7491519181d97cdcbb38c1718c1289
evgeny.rivkin <evgeny.rivkin@zerto.com>
2016-12-21 11:54:30 +0200

    [master] serving index html
    Branch: [master]

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+app.use(express.static('ui'));
+
+// app.get('/', function (req, res) {
+//     res.send('index.html');
+// });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
